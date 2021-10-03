efdd102faecb2d2442e88424a7caa6f01c0c5262
Nikita Borovkov <borovckov@yandex.ru>
2021-10-03 18:54:14 +0300

    implemented project files, added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import './App.css';
-import React from "react";
+import React, {Suspense} from "react";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import BooksLibrary from "./components/booksLibrary/BooksLibrary";
+import Preloader from "./common/preloader/Preloader";
+import {Redirect, Route, Switch} from "react-router-dom";
+import BookInfo from "./components/bookInfo/BookInfo";
+import ErrorPage from "./common/errorPage/ErrorPage";
 
 const App = () => {
-  return (
-      <div>
-        123
-      </div>
-  )
+    return (
+        <div>
+            <Header/>
+            <Suspense fallback={<Preloader/>}>
+                <Switch>
+                    <Route path='/booksLibrary'
+                           render={() => <BooksLibrary/>}/>
+                    <Route path='/bookInfo/:id'
+                           render={(props) => <BookInfo {...props}/>}/>
+                    <Route exact path='/'
+                           render={() => <Redirect to='/booksLibrary'/>}/>
+                    <Route path='*'
+                           render={() => <ErrorPage/>}/>
+                </Switch>
+            </Suspense>
+            <Footer/>
+
+        </div>
+    )
 }
 
 export default App;
