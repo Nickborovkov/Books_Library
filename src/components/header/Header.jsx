70c0fcd847801f4ee2e2cbdd37f45415ec426e55
Nikita Borovkov <borovckov@yandex.ru>
2021-10-03 18:58:39 +0300

    added header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from './header.module.css'
+import appLogo from '../../assets/images/appLogo.png'
+import {NavLink} from "react-router-dom";
+
+const Header = () => {
+    return (
+        <NavLink to='/' className={styles.header}>
+            <div className={styles.logoWrapper}>
+                <img className={styles.logo} style={{width: `40px`}} src={appLogo} alt="appLogo"/>
+            </div>
+            <h2 className={styles.title}>Google Books API app</h2>
+        </NavLink>
+    )
+}
+
+export default Header
