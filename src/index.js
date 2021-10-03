a2c33e2a965acef179d34863dcd081de2a5818b6
Nikita Borovkov <borovckov@yandex.ru>
2021-10-03 18:56:13 +0300

    HashRouter instead of BrowserRouter (for deploying to gh-pages)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {HashRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./store/store";
 
 ReactDOM.render(
-    <App />
-  ,document.getElementById('root')
+    <HashRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </HashRouter>
+    ,document.getElementById('root')
 );
